9645e105e99d80376253a7711692ebc79a4ccccd
Andres <dresjos@gmail.com>
2019-05-06 15:48:12 +0800

    Update loginPage.php

--- examples/loginPage.php
+++ examples/loginPage.php
@@ -106,7 +106,7 @@
                 <span class="navbar-toggler-bar bar3"></span>
               </button>
             </div>
-            <a class="navbar-brand" href="#pablo">Create Activity</a>
+            <a class="navbar-brand" href="#pablo">Log In</a>
           </div>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-bar navbar-kebab"></span>
